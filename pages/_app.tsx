ec3b2290c64f5a21a2ce4e4f95da750ee1f304d4
andyvy <thienvy95@gmail.com>
2022-12-03 13:56:17 +0700

    basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+
+  },[]);
+
+  if (isSSR) return null;
+
+
   return <Component {...pageProps} />
 }
+
+export default MyApp;
