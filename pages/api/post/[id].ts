49573d40cf13a73e57b4fffdc9e9cd63ea556e75
andyvy <thienvy95@gmail.com>
2023-02-05 15:28:51 +0700

    Display comment

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -29,7 +29,7 @@ export default async function handler(
                     _key: uuid(),
                     postedBy: {
                         _type: 'postedBy',
-                        _ref: userId
+                        _ref: userId, 
                     },
                 },
             ])
