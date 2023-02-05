711c1bbab534ae0770b8eb3324f548b691944a4e
andyvy <thienvy95@gmail.com>
2023-02-05 15:16:28 +0700

    Suggested Accounts

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,15 +1,33 @@
-import create from 'zustand';
+import axios from 'axios';
+import create, { SetState, StoreApi } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { IUser } from '../types';
 
-const authStore = (set: any) => ({
-    userProfile: null as IUser | null,
+import { BASE_URL } from '../utils';
+
+interface IAuthStore {
+    userProfile: IUser | null;
+    allUsers: IUser[];
+    addUser: (user: IUser) => void;
+    removeUser: () => void;
+    fetchAllUsers: () => Promise<void>;
+}
+
+const authStore = (set: StoreApi<IAuthStore>['setState']) => ({
+    userProfile: null,
+    allUsers: [],
     addUser: (user: any) => set({ userProfile: user }),
     removeUser: () => set({ userProfile: null }),
+
+    fetchAllUsers: async () => {
+        const response = await axios.get(`${BASE_URL}/api/users`)
+
+        set({ allUsers: response.data })
+    }
 });
 
 const useAuthStore = create(
-    persist(authStore, {
+    persist<IAuthStore>(authStore, {
         name: 'auth'
     })
 )
